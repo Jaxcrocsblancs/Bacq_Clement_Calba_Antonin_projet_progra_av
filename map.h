9cc275e97dcaa772cf9e6d68ba1a8e05479ff44d
Clément BACQ <Clment BACQ>
2016-10-13 16:49:58 +0200

    fctin map modif

--- map.h
+++ map.h
@@ -14,36 +14,35 @@
 #define COL 20
 #define LIG 20
 
-struct map_objt
+struct sol
 {
 	SDL_Surface s;
-	int a;
+	int id;
 };
-typedef struct map_objt map_objt;
+typedef struct sol sol;
 
 struct NODE
 {
 	int walkable; // si on a accès a la case
 
-	int onopen; // liste ouverte
-	int onclosed; // liste fermé
-
 	int g; // cout de l'opération a un temps t
 	int h; // cout restant des opération a un temps t
 	int f; // cout total
-
+	int onclosed;
 	int parentx;
 	int parenty;
 }; // tableau representant la map
 
 typedef struct NODE NODE;
 
+liste_point chemin(sol tab[COL][LIG],int startx, int starty, int endx, int endy);
+
 liste_point findpath(int startx, int starty, int endx, int endy,NODE node[COL][LIG]);
 liste_point triPath(liste_point L,NODE node[COL][LIG]);
-void initnodes(map_objt tab[COL][LIG],NODE node[COL][LIG]);
-int Walkable(map_objt tab[COL][LIG],int col, int lig);
-void remplisTab(int tab[COL][LIG]);
-liste_point Astar(map_objt tab[COL][LIG], NODE node[COL][LIG],int startx, int starty, int endx, int endy);
+void initnodes(sol tab[COL][LIG],NODE node[COL][LIG]);
+int Walkable(sol tab[COL][LIG],int col, int lig);
+void remplisTab(sol tab[COL][LIG]);
+liste_point Astar(sol tab[COL][LIG], NODE node[COL][LIG],int startx, int starty, int endx, int endy);
 
 
 #endif /* MAP_H_ */
